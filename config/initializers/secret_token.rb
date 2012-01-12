476073cd9a6ae66036b0f5ba2191cbf0941397d8
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2012-01-12 10:21:34 -0300

    Starting the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firewall::Application.config.secret_token = 'b32d2d0e9ef4818dcd3f8b832b61f9ba6db745249bd40c422e8042dbc5fbe587320018281231060057a62409c08699ff19af5576e77c61030f4e4b63f3eff43d'
